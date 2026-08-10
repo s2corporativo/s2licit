d928efa2c53e10eb6f9db6ff318f344f3d970d01
S2 CORPORATIVO <adm@vetmg.com.br>
2026-08-10 08:43:24 -0300

    chore(proposals): remove unused lifecycle import

--- server/services/proposalLifecycleService.ts
+++ server/services/proposalLifecycleService.ts
@@ -1,5 +1,5 @@
 import { TRPCError } from "@trpc/server";
-import { and, eq, inArray, isNull, sql } from "drizzle-orm";
+import { and, eq, isNull, sql } from "drizzle-orm";
 import {
   financialEntries,
   funilOportunidades,
